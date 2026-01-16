f126dd0c9f6a0d5ca153c6f24d6c4c5f8a0aadfb
John Truckenbrodt <john.truckenbrodt@posteo.de>
2026-01-16 10:10:28 +0100

    [scene_select] perform antimeridian filtering on main search result

--- cesard/search.py
+++ cesard/search.py
@@ -339,6 +339,14 @@ def scene_select(
         - the list of MGRS tiles
     
     """
+    def filter_antimeridian(selection, index):
+        # The geometry of scenes crossing the antimeridian is stored as multipolygon.
+        out = [x for x in selection if x[index].startswith('POLYGON')]
+        if len(out) < len(selection):
+            log.debug(f'removed {len(selection) - len(out)} '
+                      f'scene(s) crossing the antimeridian')
+        return out
+    
     args = kwargs.copy()
     if 'mindate' in args.keys():
         args['mindate'] = date_to_utc(args['mindate'], as_datetime=True)
@@ -393,19 +401,16 @@ def scene_select(
     if vec is None:
         log.debug("performing initial scene search without geometry constraint")
         args['return_value'] = ['mindate', 'maxdate', 'geometry_wkt']
-        selection_tmp = archive.select(**args)
-        log.debug(f'got {len(selection_tmp)} scenes')
-        if len(selection_tmp) == 0:
+        selection = archive.select(**args)
+        log.debug(f'got {len(selection)} scenes')
+        if len(selection) == 0:
             return [], []
-        mindates, maxdates, geometries_init = zip(*selection_tmp)
-        # The geometry of scenes crossing the antimeridian is stored as multipolygon.
-        # Since the processor is currently not able to process these scenes, they are
-        # removed in this step.
-        geometries = [x for x in geometries_init if x.startswith('POLYGON')]
-        if len(geometries) < len(geometries_init):
-            log.debug(f'removed {len(geometries_init) - len(geometries)} '
-                      f'scenes crossing the antimeridian')
-        del selection_tmp
+        # Since the processor is currently not able to process scenes
+        # crossing the antimeridian, they are removed in this step.
+        selection = filter_antimeridian(selection, index=2)
+        
+        mindates, maxdates, geometries = zip(*selection)
+        del selection
         
         log.debug(f"loading geometries")
         scenes_geom = [wkt2vector(x, srs=4326) for x in geometries]
@@ -431,7 +436,7 @@ def scene_select(
         args['maxdate'] += timedelta(minutes=1)
     
     args['return_value'] = return_values.copy()
-    for key in ['mindate', 'maxdate']:
+    for key in ['mindate', 'maxdate', 'geometry_wkt']:
         if key not in args['return_value']:
             args['return_value'].append(key)
     
@@ -440,6 +445,10 @@ def scene_select(
         args['vectorobject'] = combined
         selection = archive.select(**args)
     del vec
+    
+    # second antimeridian filtering on the main search result
+    rv_geom_key = args['return_value'].index('geometry_wkt')
+    selection = filter_antimeridian(selection, index=rv_geom_key)
     
     # reduce the selection to the time range defined by the user
     if mindate_init is not None or maxdate_init is not None:
